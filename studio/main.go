4e8ca08587e50eae27e6e642d34d32b7d5ae11a0
BZZ <bzzs80368@gmail.com>
2026-02-05 19:46:03 +0300

    Add files via upload

---
+++ studio/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type ToDoStructur struct {
+	string zagalovok
+	string info
+	float64 time
+}
+
+func main() {
+	
+	// for {
+		ToDoMap := make(map[string]{ToDomaToDoStructur})
+		
+		
+	// }
+}
